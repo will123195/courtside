c06306ec12540312a9401faa17b35816d86c6b07
will123195 <will123195@gmail.com>
2013-05-10 12:11:17 -0400

    fix

--- pages/default/default.php
+++ pages/default/default.php
@@ -8,10 +8,13 @@ $this->template('html5', 'top');
 
 ?>
 
-<h1><?=$this->title?></h1>
+<div class="main">
+	<h1><?=$this->title?></h1>
+	<h2>Who needs courtside tickets when you've got this...</h2>
 
-<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=%23BetterThanCourtside" data-widget-id="332880047230828545">Tweets about "#BetterThanCourtside"</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+	<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=%23BetterThanCourtside" data-widget-id="332880047230828545">Tweets about "#BetterThanCourtside"</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
 
 
 <?
