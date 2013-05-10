f1f0a03117f9cf8bbcd577efa01066831d7955f4
will123195 <will123195@gmail.com>
2013-05-10 11:33:56 -0400

    initial page

---
+++ pages/default/default.php
@@ -0,0 +1,20 @@
+<?php
+
+$this->title = '#BetterThanCourtside';
+
+$this->css[] 'http://fonts.googleapis.com/css?family=Oswald:700';
+
+$this->template('html5', 'top');
+
+?>
+
+<h1><?=$this->title?></h1>
+
+<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/search?q=%23BetterThanCourtside" data-widget-id="332880047230828545">Tweets about "#BetterThanCourtside"</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+<?
+
+
+$this->template('html5', 'bottom');
